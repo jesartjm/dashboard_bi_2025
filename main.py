be8c15b5620fdbabc72af8b702a837d4e62c079e
jesartjm <0241607@up.edu.mx>
2025-12-08 21:49:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.caption("Universidad Panamericana · Campus CDMX")
 # -----------------------------------------------------------
 @st.cache_data
 def load_data():
-    url='UBER%20dataset.xlsx'
+    url="UBER%20dataset.xlsx"
     all_sheets = pd.read_excel(url, sheet_name=None)
     return all_sheets['Switchbacks']
 df = load_data()
